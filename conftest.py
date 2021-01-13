611b2016a8409906e0f06eca93a0ff5160d3fe0e
Dmitriy Bormotov <squier7@gmail.com>
2021-01-13 17:30:35 +0500

    switch back to chrome browser

--- conftest.py
+++ conftest.py
@@ -1,13 +1,12 @@
 import pytest
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
-from webdriver_manager.utils import ChromeType
 import allure
 
 
 @pytest.fixture(autouse=True, scope="function")
 def browser(request):
-    driver = webdriver.Chrome(ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install())
+    driver = webdriver.Chrome(ChromeDriverManager().install())
     driver.implicitly_wait(10)
     yield driver
     if request.node.rep_call.failed:
